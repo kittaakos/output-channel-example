200b8290113e39ce592921a041f599c303fc98e0
Akos Kitta <kittaakos@typefox.io>
2020-06-30 14:51:09 +0200

    init
    Signed-off-by: Akos Kitta <kittaakos@typefox.io>

---
+++ src/extension.ts
@@ -0,0 +1,22 @@
+import * as vscode from 'vscode';
+
+export function activate(context: vscode.ExtensionContext) {
+    console.log('Example for https://github.com/eclipse-theia/theia/discussions/8085');
+    context.subscriptions.push(vscode.commands.registerCommand('hello-world-example-generated', (...args: any[]) => {
+        vscode.window.showInformationMessage('Hello!');
+    }));
+    context.subscriptions.push(vscode.commands.registerCommand('start-debugging', async (...args: any[]) => {
+        const testChannel = vscode.window.createOutputChannel('test-channel');
+        testChannel.appendLine('starting...');
+        await vscode.debug.startDebugging(vscode.workspace.workspaceFolders![0], {
+            "type": "node",
+            "request": "launch",
+            "name": "Launch Program",
+            "program": "${workspaceFolder}/theia-hello-world-plugin/node-test/index.js",
+        });
+        testChannel.appendLine('running...');
+        testChannel.appendLine('running...');
+        testChannel.appendLine('running...');
+        testChannel.show(true);
+    }));
+}
